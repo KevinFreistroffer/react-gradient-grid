b7320e427719c14b094143d91b27521a24082993
Kevin <kevin.freistroffer@gmail.com>
2020-08-31 22:46:20 -0700

    build

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, './build')));
 
 app.get('*', (req, res, next) => {
-  res.sendFile(express.static(path.join(__dirname, './public/index.html')));
+  res.sendFile(express.static(path.join(__dirname, './build/index.html')));
 });
 
 app.listen(process.env.PORT, () => {
