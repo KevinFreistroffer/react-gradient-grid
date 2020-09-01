a12b600af6fd54b521f19d1d25c148727edf6c26
Kevin <kevin.freistroffer@gmail.com>
2020-08-31 22:41:56 -0700

    edited server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, './public')));
 
+app.get('*', (req, res, next) => {
+  res.sendFile(express.static(path.join(__dirname, './public/index.html')));
+});
+
 app.listen(process.env.PORT, () => {
   console.log('Server started.');
 });
